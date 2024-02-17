2cdf6000951af72e1aa15044613df0ac6b76e8fe
Jesús Rojas <jrojasmorales97@gmail.com>
2024-02-17 16:36:17 +0100

    Mostrando proyectos en forma de lista en la pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjectDetail } from './pages/project-detail/project-detail.component';
 import { About } from './pages/about/about.component';
 import { Projects } from './pages/projects/projects.component';
 import { Contact } from './pages/contact/contact.component';
@@ -12,11 +11,6 @@ const routes: Routes = [
         path: '',
         component: Projects,
         canActivate: [PageToggle],
-    },
-    {
-        path: 'projects/:id',
-        component: ProjectDetail,
-        canActivate: [PageToggle]
     },
     {
         path: 'about',
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,7 +2,6 @@ import { Projects } from './projects/projects.component';
 import { About } from './about/about.component';
 import { Contact } from './contact/contact.component';
 import { Inspiration } from './inspiration/inspiration.component';
-import { ProjectDetail } from './project-detail/project-detail.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
@@ -10,7 +9,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [Projects, About, Contact, Inspiration, ProjectDetail],
+    declarations: [Projects, About, Contact, Inspiration],
     imports: [ CommonModule, ComponentsModule, ReactiveFormsModule, RouterModule ],
 })
 export class PagesModule {}
