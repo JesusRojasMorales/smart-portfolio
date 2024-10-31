87489b7a810bb131ad7a33c754dc0bbdb944f44b
Jesús Rojas <jrojasmorales97@gmail.com>
2024-10-31 19:08:11 +0100

    Angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,27 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    ComponentsModule,
-    CommonModule,
-    HttpClientModule,
-    BrowserModule,
-    AppRoutingModule,
-    PagesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+@NgModule({ 
+    declarations: [
+        AppComponent,
+    ],
+    bootstrap: [
+        AppComponent
+    ], 
+    imports: [
+        ComponentsModule,
+        CommonModule,
+        BrowserModule,
+        AppRoutingModule,
+        PagesModule
+    ], 
+    providers: [
+        provideHttpClient(withInterceptorsFromDi())
+    ] 
 })
 export class AppModule { }
